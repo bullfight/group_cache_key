9fd63b14c3b6c21fc06f56d68494a7eb8550363d
Patrick Schmitz <p.schmitz@gmail.com>
2012-09-21 01:00:40 -0500

    recompose and clean up gem

--- test/group_cache_key/group_cache_key_test.rb
+++ test/group_cache_key/group_cache_key_test.rb
@@ -22,11 +22,13 @@ class Widget < ActiveRecord::Base
   end
 end
 
-class GroupCacheKeyTest < Test::Unit::TestCase
+class GroupCacheKeyTest < MiniTest::Unit::TestCase
+
   def test_empty_collection_should_return_unique_string_representation
     @widgets = Widget.empty_find
     assert_equal("empty/#{@widgets.object_id}", @widgets.cache_key)
   end
+
   def test_collection_should_return_unique_string_with_count_hash_and_timestamps
     @widgets = Widget.find
     hash = Digest::MD5.hexdigest(@widgets.collect{|w| w.id}.to_s)
@@ -34,4 +36,5 @@ class GroupCacheKeyTest < Test::Unit::TestCase
     updated = @widgets.first.updated_at.to_i.to_s
     assert_equal("widgets/#{@widgets.length}-#{hash}-#{created}-#{updated}", @widgets.cache_key)
   end
+
 end
